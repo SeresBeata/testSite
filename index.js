b6fe464a79a5af2dd1ed64f2610b207f14844d1d
MolnarBeata <115145107+SeresBeata@users.noreply.github.com>
2023-11-05 19:08:21 +0100

    Update port at index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello! This is my test site!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(port, "0.0.0.0", () => {
+  console.log("Listening on Port " + port);
 });
